31f5d2078117c13d12586d0c194753357e9ec5d7
Jose Alberto Llucia <55691458+llucia1@users.noreply.github.com>
2025-07-14 12:44:35 +0200

    Update PostRepository.php

--- src/Post/Infraestructure/PostRepository.php
+++ src/Post/Infraestructure/PostRepository.php
@@ -18,17 +18,6 @@ class PostRepository extends baseApiRepository implements PostRepoContract
     {
         try {
             $this->persist($post);
-            /* 
-                Si fuera un model de Eloquent:
-                        $this->model->id = $post->id();
-                        $this->model->userId = $post->userId();
-                        $this->model->title = $post->title();
-                        $this->model->body = $post->body();
-                        $this->save($this->model);
-            */
-            // Podemos crear una entidad Post de dominio y devolverlo. Si creamos un servicio de dominio, 
-            // por ejemplo llamado 'new' o 'created', este serrvicio puede lanzar un evento o lo agregamos a un array de eventos para lanzalos 
-            // cuando consideremos oportuno. Estos eventos pertenecen a negocio, es decir lo contendra el metodo del servicio de dominio.
         } catch (\Throwable $th) {
             throw new crearPostInBDException('Error when inserting a new Post in the Database.') ;
         }
